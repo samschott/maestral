fee9c50251eee23a03e5c6c1378e758b958bbc7e
Sam Schott <ss2151@cam.ac.uk>
2018-12-09 21:19:05 +0000

    bug fix
    fixed a crash on inital configuration if specified Dropbox folder already exists

--- maestral/__init__.py
+++ maestral/__init__.py
@@ -1,7 +1,7 @@
 from .client import MaestralClient
 from .main import Maestral
 try:
-    import pyqt5
+    import PyQt5
     from .gui.main import MaestralApp
 except ImportError:
     print('Warning: PyQt5 is required to run the Maestral GUI. Run `pip install pyqt5` to install it.')
