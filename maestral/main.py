4706f18e4536584c10d08a19bd4012238eefeef1
Sam Schott <ss2151@cam.ac.uk>
2019-06-25 12:25:26 +0100

    version bump to v0.1.2-dev1

--- maestral/main.py
+++ maestral/main.py
@@ -6,7 +6,7 @@ Created on Wed Oct 31 16:23:13 2018
 @author: samschott
 """
 
-__version__ = "0.1.2-dev0"
+__version__ = "0.1.2-dev1"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/SamSchott/maestral"
 
