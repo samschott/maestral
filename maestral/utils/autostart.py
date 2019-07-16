06b7b85b50683f3986c4a9cf091447ddf5eb1b07
Sam Schott <ss2151@cam.ac.uk>
2019-07-16 12:09:46 +0100

    create launch files on-demand

--- maestral/utils/autostart.py
+++ maestral/utils/autostart.py
@@ -19,15 +19,19 @@ class AutoStart(object):
 
     def __init__(self):
         self.system = platform.system()
-        if getattr(sys, "frozen", False) and self.system == "Darwin":
+        if self.system == "Darwin":
             self.filename = "com.samschott.maestral.plist"
             self.destination_dir = os.path.expanduser("~/Library/LaunchAgents")
-        elif self.system == "Darwin":
-            self.filename = "com.maestral.loginscript.plist"
-            self.destination_dir = os.path.expanduser("~/Library/LaunchAgents")
+            if getattr(sys, "frozen", False):
+                launch_command = "/Applications/Maestral.app/Contents/MacOS/main"
+            else:
+                launch_command = "mercury-gui"
+            self.contents = _plist_template.format(launch_command)
         elif self.system == "Linux":
             self.filename = "maestral.desktop"
             self.destination_dir = os.path.expanduser("~/.config/autostart")
+            launch_command = "mercury-gui"
+            self.contents = _desktop_entry_template.format(launch_command)
         else:
             raise OSError("Windows is not currently supported.")
 
@@ -38,7 +42,8 @@ class AutoStart(object):
         if not os.path.isdir(self.destination_dir):
             os.makedirs(self.destination_dir)
 
-        shutil.copyfile(self.source, self.destination)
+        with open(self.destination, "w+") as f:
+            f.write(self.contents)
 
     def disable(self):
         if os.path.exists(self.destination):
@@ -47,3 +52,37 @@ class AutoStart(object):
     @property
     def enabled(self):
         return os.path.isfile(self.destination)
+
+
+_plist_template = """
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Label</key>
+	<string>com.samschott.maestral</string>
+	<key>ProcessType</key>
+	<string>Interactive</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>{0}</string>
+	</array>
+	<key>RunAtLoad</key>
+	<true/>
+</dict>
+</plist>
+"""
+
+_desktop_entry_template = """
+[Desktop Entry]
+Name=Maestral
+GenericName=File Synchronizer
+Comment=Sync your files with Dropbox
+Exec={0}
+Hidden=false
+Terminal=false
+Type=Application
+Categories=Network;FileTransfer;
+StartupNotify=false
+X-GNOME-Autostart-enabled=true
+"""
