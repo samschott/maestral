b42cd871fc6b50b95e917d9366c4c09636095868
Sam Schott <ss2151@cam.ac.uk>
2019-08-06 14:30:04 +0100

    added warning message for python requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
+import sys
 from setuptools import setup, find_packages
+
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 6)
+
+# This check and everything above must remain compatible with Python 2.7.
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write("""
+==========================
+Unsupported Python version
+==========================
+Maestral requires Python {}.{}, but you're trying to install it on
+Python {}.{}. This may be because you are using a version of pip that
+doesn't understand the python_requires classifier. Make sure you
+have pip >= 9.0 and setuptools >= 24.2, then try again:
+    $ python3 -m pip install --upgrade pip setuptools
+    $ python3 -m pip install maestral
+
+""".format(*(REQUIRED_PYTHON + CURRENT_PYTHON)))
+    sys.exit(1)
 
 
 def get_version(relpath):
