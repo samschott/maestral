253fa60ecce3221f180afb0eceff3a5c90c65a25
Sam Schott <ss2151@cam.ac.uk>
2019-08-10 19:22:55 +0100

    added method mask_image

--- maestral/gui/utils.py
+++ maestral/gui/utils.py
@@ -11,6 +11,8 @@ import platform
 from subprocess import Popen
 from traceback import format_exception
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QRect
+from PyQt5.QtGui import QBrush, QImage, QPainter, QPixmap, QWindow
 
 from maestral.gui.resources import APP_ICON_PATH
 from maestral.utils import is_macos_bundle
@@ -297,3 +299,51 @@ def quit_and_restart_maestral():
             pid, config_name), shell=True)
 
     QtCore.QCoreApplication.quit()
+
+
+def mask_image(imgdata, imgtype='jpeg', size=64):
+    """Return a ``QPixmap`` from *imgdata* masked with a smooth circle.
+
+    *imgdata* are the raw image bytes, *imgtype* denotes the image type.
+
+    The returned image will have a size of *size* × *size* pixels.
+
+    """
+    # Load image and convert to 32-bit ARGB (adds an alpha channel):
+    image = QImage.fromData(imgdata, imgtype)
+    image.convertToFormat(QImage.Format_ARGB32)
+
+    # Crop image to a square:
+    imgsize = min(image.width(), image.height())
+    rect = QRect(
+        (image.width() - imgsize) / 2,
+        (image.height() - imgsize) / 2,
+        imgsize,
+        imgsize,
+    )
+    image = image.copy(rect)
+
+    # Create the output image with the same dimensions and an alpha channel
+    # and make it completely transparent:
+    out_img = QImage(imgsize, imgsize, QImage.Format_ARGB32)
+    out_img.fill(Qt.transparent)
+
+    # Create a texture brush and paint a circle with the original image onto
+    # the output image:
+    brush = QBrush(image)        # Create texture brush
+    painter = QPainter(out_img)  # Paint the output image
+    painter.setBrush(brush)      # Use the image texture brush
+    painter.setPen(Qt.NoPen)     # Don't draw an outline
+    painter.setRenderHint(QPainter.Antialiasing, True)  # Use AA
+    painter.drawEllipse(0, 0, imgsize, imgsize)  # Actually draw the circle
+    painter.end()                # We are done (segfault if you forget this)
+
+    # Convert the image to a pixmap and rescale it.  Take pixel ratio into
+    # account to get a sharp image on retina displays:
+    pr = QWindow().devicePixelRatio()
+    pm = QPixmap.fromImage(out_img)
+    pm.setDevicePixelRatio(pr)
+    size *= pr
+    pm = pm.scaled(size, size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+    return pm
