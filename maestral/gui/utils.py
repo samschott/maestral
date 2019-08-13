3fea208b3b8d4c56394e63e48d13442be95731ca
Sam Schott <ss2151@cam.ac.uk>
2019-08-13 13:56:23 +0100

    fixed rounding error in `get_scaled_font`

--- maestral/gui/utils.py
+++ maestral/gui/utils.py
@@ -59,7 +59,7 @@ def get_scaled_font(scaling=1.0, bold=False, italic=False):
     font = label.font()
     font.setBold(bold)
     font.setItalic(italic)
-    font_size = font.pointSize()*scaling
+    font_size = round(font.pointSize()*scaling)
     # noinspection PyTypeChecker
     font.setPointSize(font_size)
 
