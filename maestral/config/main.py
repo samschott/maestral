65ff53a7d8a86b4306fc127bbdabb3fbf05d5613
Sam Schott <ss2151@cam.ac.uk>
2019-08-15 11:09:37 +0100

    set default log file level to INFO

--- maestral/config/main.py
+++ maestral/config/main.py
@@ -49,7 +49,7 @@ DEFAULTS = [
             ('app',
              {
               'notifications': True,
-              'log_level_file': 40,
+              'log_level_file': 20,
               'log_level_console': 20,
               }),
             ('internal',
