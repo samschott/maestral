48086a08edd40b698c17a0ed234f2098d3aabeb4
SamSchott <ss2151@cam.ac.uk>
2019-10-28 17:39:30 +0000

    set fallback return value for XDG_DATA_DIRS

--- maestral/gui/__init__.py
+++ maestral/gui/__init__.py
@@ -9,5 +9,5 @@ import os
 
 # add "~/.local/share" to XDG_DATA_DIRS
 # this is needed to find icons installed for the current user only
-XDG_DATA_DIRS = os.environ.get("XDG_DATA_DIRS") + ":" + os.path.expanduser("~/.local/share")
+XDG_DATA_DIRS = os.environ.get("XDG_DATA_DIRS", "") + ":" + os.path.expanduser("~/.local/share")
 os.environ["XDG_DATA_DIRS"] = XDG_DATA_DIRS
