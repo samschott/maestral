97aa3f9c70e1b20ca42e0b8c7cb02cb3c9c12957
Sam Schott <ss2151@cam.ac.uk>
2019-11-03 14:39:05 +0000

    bump version to 0.4.4-dev0

--- maestral/__init__.py
+++ maestral/__init__.py
@@ -5,6 +5,6 @@ Created on Wed Oct 31 16:23:13 2018
 
 @author: samschott
 """
-__version__ = "0.4.3"
+__version__ = "0.4.4-dev0"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/SamSchott/maestral"
