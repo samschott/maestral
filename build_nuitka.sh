be01e35c873e5239de6995bfc0d3ad85f5d78c63
Sam Schott <ss2151@cam.ac.uk>
2020-01-29 21:50:39 +0000

    removed nuitka build script

--- build_nuitka.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python3 -OO -m nuitka --follow-imports --standalone --enable-plugin=qt-plugins=sensible,styles --plugin-enable=multiprocessing --lto maestral/gui/main.py
-
-# python3 post_build.py
