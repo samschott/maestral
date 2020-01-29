884bc54213c0e6b4da844c9956ed2d011b8ce8a1
Sam Schott <ss2151@cam.ac.uk>
2020-01-29 21:02:15 +0000

    move macOS libs to where they can be discovered

--- build_pyi.sh
+++ build_pyi.sh
@@ -8,20 +8,30 @@ NEW_BUILD=$(($OLD_BUILD+1))
 OLD="'CFBundleVersion': '$OLD_BUILD',"
 NEW="'CFBundleVersion': '$NEW_BUILD',"
 
-echo "*********** INCREMENTING BUILD NUMBER TO $NEW_BUILD ***********"
+echo "**** INCREMENTING BUILD NUMBER TO $NEW_BUILD ***********"
 
 sed -i "" "s/$OLD/$NEW/g" $SPEC_FILE
 
-echo "********************* BUILDING ************************"
+echo "**** BUILDING ******************************************"
 
 python3 -OO -m PyInstaller  -y --clean -w $SPEC_FILE
 
-echo "*************** REMOVING QML MODULES ******************"
+echo "**** REMOVING UNNEEDED MODULES *************************"
 
 python3 post_build.py
 
-echo "********************** SIGNING ************************"
+echo "**** MOVING COCOA LIBS *********************************"
+
+mv dist/Maestral.app/Contents/MacOS/Foundation/_Foundation.cpython-37m-darwin.so dist/Maestral.app/Contents/MacOS/Foundation.so
+mv dist/Maestral.app/Contents/MacOS/CoreFoundation/_CoreFoundation.cpython-37m-darwin.so dist/Maestral.app/Contents/MacOS/CoreFoundation.so
+mv dist/Maestral.app/Contents/MacOS/AppKit/_AppKit.cpython-37m-darwin.so dist/Maestral.app/Contents/MacOS/AppKit.so
+
+rm -R -f dist/Maestral.app/Contents/MacOS/Foundation/
+rm -R -f dist/Maestral.app/Contents/MacOS/CoreFoundation/
+rm -R -f dist/Maestral.app/Contents/MacOS/AppKit/
+
+echo "**** SIGNING ******************************************"
 
 codesign -s "Apple Development: sam.schott@outlook.com (FJNXBRUVWL)" --deep dist/Maestral.app
 
-echo "*********************** DONE **************************"
+echo "**** DONE *********************************************"
