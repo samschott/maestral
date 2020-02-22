19c24d80a9388ed1bacdf84488488827e04cc0bd
Sam Schott <ss2151@cam.ac.uk>
2020-02-22 13:49:43 +0000

    don't import maestral modules in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,98 @@
 import sys
 import os
+import os.path as osp
+import platform
+import tempfile
 from setuptools import setup, find_packages
 from maestral import __version__, __author__, __url__
-from maestral.config.base import list_configs
-from maestral.utils.appdirs import get_runtime_path, get_old_runtime_path
 
-CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 6)
 
 # check for running daemons before updating to prevent
 # incompatible versions of CLI / GUI and daemon
 
+def get_home_dir():
+    try:
+        path = osp.expanduser('~')
+    except Exception:
+        path = ''
+
+    if osp.isdir(path):
+        return path
+    else:
+        for env_var in ('HOME', 'USERPROFILE', 'TMP'):
+            path = os.environ.get(env_var, '')
+            if osp.isdir(path):
+                return path
+            else:
+                path = ''
+
+        if not path:
+            raise RuntimeError('Please set the environment variable HOME to '
+                               'your user/home directory.')
+
+
+_home_dir = get_home_dir()
+
+
+def _to_full_path(path, subfolder, filename, create):
+
+    if subfolder:
+        path = osp.join(path, subfolder)
+
+    if create:
+        os.makedirs(path, exist_ok=True)
+
+    if filename:
+        path = osp.join(path, filename)
+
+    return path
+
+
+def get_conf_path(subfolder=None, filename=None, create=True):
+    if platform.system() == 'Darwin':
+        conf_path = osp.join(get_home_dir(), 'Library', 'Application Support')
+    else:
+        fallback = osp.join(get_home_dir(), '.config')
+        conf_path = os.environ.get('XDG_CONFIG_HOME', fallback)
+
+    return _to_full_path(conf_path, subfolder, filename, create)
+
+
+def get_runtime_path(subfolder=None, filename=None, create=True):
+    if platform.system() == 'Darwin':
+        runtime_path = get_conf_path(create=False)
+    else:
+        fallback = os.environ.get('XDG_CACHE_HOME', osp.join(_home_dir, '.cache'))
+        runtime_path = os.environ.get('XDG_RUNTIME_DIR', fallback)
+
+    return _to_full_path(runtime_path, subfolder, filename, create)
+
+
+def get_old_runtime_path(subfolder=None, filename=None, create=True):
+    if platform.system() == 'Darwin':
+        runtime_path = tempfile.gettempdir()
+    else:
+        fallback = os.environ.get('XDG_CACHE_HOME', osp.join(_home_dir, '.cache'))
+        runtime_path = os.environ.get('XDG_RUNTIME_DIR', fallback)
+
+    return _to_full_path(runtime_path, subfolder, filename, create)
+
+
+def list_configs():
+    configs = []
+    for file in os.listdir(get_conf_path('maestral')):
+        if file.endswith('.ini'):
+            configs.append(os.path.splitext(os.path.basename(file))[0])
+
+    return configs
+
+
 running_daemons = []
 
 for config in list_configs():
-    pid_file = get_runtime_path("maestral", config + ".pid")
-    old_pid_file = get_old_runtime_path("maestral", config + ".pid")
-    if os.path.exists(pid_file) or os.path.exists(old_pid_file):
+    pid_file = get_runtime_path('maestral', config + '.pid')
+    old_pid_file = get_old_runtime_path('maestral', config + '.pid')
+    if osp.exists(pid_file) or osp.exists(old_pid_file):
         running_daemons.append(config)
 
 if running_daemons:
@@ -30,6 +106,8 @@ of config files and compatibility been the CLI and daemon.
     """)
     sys.exit(1)
 
+
+# proceed with actual install
 
 setup(
     name='maestral',
