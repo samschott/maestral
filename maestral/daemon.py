433d530b8528160fe460910f8c72279eae1d03f7
Sam Schott <ss2151@cam.ac.uk>
2020-03-02 21:58:22 +0000

    simplify startup script

--- maestral/daemon.py
+++ maestral/daemon.py
@@ -272,7 +272,7 @@ def start_maestral_daemon_process(config_name='maestral', run=True):
         cc = quote(cc)
         r = bool(r)
         subprocess.Popen(
-            [sys.executable, '-c', f'from maestral.daemon import run_maestral_daemon; run_maestral_daemon("{cc}", {r})'],
+            [sys.executable, '-c', f'import maestral.daemon; maestral.daemon.run_maestral_daemon("{cc}", {r})'],
             stdin=STD_IN_OUT, stdout=STD_IN_OUT, stderr=STD_IN_OUT,
         )
 
