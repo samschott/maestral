e79fd8a821b4ed08388b88476a5b630a3c82f0c5
Sam Schott <ss2151@cam.ac.uk>
2020-03-02 23:22:41 +0000

    released dev5 for PyPI test

--- maestral/__init__.py
+++ maestral/__init__.py
@@ -19,6 +19,6 @@ The following APIs should remain stable for front ends:
 
 """
 
-__version__ = "0.6.2.dev4"
+__version__ = "0.6.2.dev5"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/SamSchott/maestral"
