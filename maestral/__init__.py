50ae4fde9ab150064dbf3772805a33451613d8e2
Sam Schott <ss2151@cam.ac.uk>
2020-03-15 23:05:15 +0000

    bump to dev8

--- maestral/__init__.py
+++ maestral/__init__.py
@@ -19,6 +19,6 @@ The following APIs should remain stable for front ends:
 
 """
 
-__version__ = "0.6.2.dev7"
+__version__ = "0.6.2.dev8"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/SamSchott/maestral"
