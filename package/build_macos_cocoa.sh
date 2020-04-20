02dd51d6286f97ed9a8cc698c475fcda336e29ac
Sam Schott <ss2151@cam.ac.uk>
2020-04-20 21:33:12 +0200

    [package] build pyinstaller bootloader with MACOSX_DEPLOYMENT_TARGET=10.13
    - this fixes several issues, among them macOS proper darkmode support
    - see https://github.com/pyinstaller/pyinstaller/issues/4627

--- package/build_macos_cocoa.sh
+++ package/build_macos_cocoa.sh
@@ -3,6 +3,22 @@
 SPEC_FILE=maestral_macos_cocoa.spec
 BUILD_NO=$(grep -E -o "[0-9]*" bundle_version_macos.txt)
 
+echo "**** BUILDING PYINSTALLER ******************************"
+
+git clone https://github.com/pyinstaller/pyinstaller.git build/pyinstaller
+cd build/pyinstaller
+git checkout develop
+cd bootloader
+export MACOSX_DEPLOYMENT_TARGET=10.13
+export CFLAGS=-mmacosx-version-min=10.13
+export CPPFLAGS=-mmacosx-version-min=10.13
+export LDFLAGS=-mmacosx-version-min=10.13
+export LINKFLAGS=-mmacosx-version-min=10.13
+python ./waf all
+cd ..
+pip install .
+cd ../..
+
 echo "**** BUILD NUMBER $BUILD_NO ****************************"
 
 python3 -m PyInstaller  -y --clean -w $SPEC_FILE
