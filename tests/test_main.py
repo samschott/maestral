88c10ca706e774ac4b7ed0e824c5ab0c34331705
Sam Schott <ss2151@cam.ac.uk>
2020-05-05 14:53:43 +0200

    [tests] added dummy api test

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Sam Schott  (ss2151@cam.ac.uk)
+
+(c) Sam Schott; This work is licensed under the MIT licence.
+
+"""
+import unittest
+from unittest import TestCase
+
+
+class TestAPI(TestCase):
+
+    def test_selective_sync(self):
+        pass
+
+    def test_sync_issues(self):
+        pass
+
+    def test_mignore(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
