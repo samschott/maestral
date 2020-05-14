a34b113501298a8cd8ad49927ad8a19f90b2fef7
Sam Schott <ss2151@cam.ac.uk>
2020-05-14 14:56:17 +0200

    bumped to 1.0.3.beta1

--- maestral/__init__.py
+++ maestral/__init__.py
@@ -16,6 +16,6 @@ The following APIs should remain stable for frontends:
 
 """
 
-__version__ = '1.0.3.dev1'
+__version__ = '1.0.3.beta1'
 __author__ = 'Sam Schott'
 __url__ = 'https://github.com/SamSchott/maestral'
