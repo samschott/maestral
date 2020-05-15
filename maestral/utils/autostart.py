936f7a4c94c6f366b7accaa5b8596ba241f2f6b1
Sam Schott <ss2151@cam.ac.uk>
2020-05-15 23:33:06 +0200

    [autostart] create ~/Library/LaunchAgents if it does not exist

--- maestral/utils/autostart.py
+++ maestral/utils/autostart.py
@@ -219,8 +219,9 @@ class AutoStartLaunchd(AutoStartMaestralBase):
 
         with open(osp.join(_resources, 'com.samschott.maestral.plist')) as f:
             plist_template = f.read()
-
-        self.destination = osp.join(get_home_dir(), 'Library', 'LaunchAgents', filename)
+        
+        self.path = osp.join(get_home_dir(), 'Library', 'LaunchAgents')
+        self.destination = osp.join(self.path, filename)
 
         arguments = [f'\t\t<string>{arg}</string>' for arg in self.start_cmd]
 
@@ -230,6 +231,8 @@ class AutoStartLaunchd(AutoStartMaestralBase):
         )
 
     def _enable(self):
+        os.makedirs(self.path, exist_ok=True)
+        
         with open(self.destination, 'w+') as f:
             f.write(self.contents)
 
