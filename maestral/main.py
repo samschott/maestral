b17943b2928c2866c74dadf626341cf5cdbe8e6b
Sam Schott <ss2151@cam.ac.uk>
2020-07-03 16:10:58 +0200

    simplify linked check

--- maestral/main.py
+++ maestral/main.py
@@ -549,16 +549,13 @@ class Maestral:
         """Bool indicating if Maestral is linked to a Dropbox account (read only). This
         will block until the user's keyring is unlocked to load the saved auth token."""
 
-        # getting tokens triggers keyring access on first call
-        token = self._auth.refresh_token or self._auth.access_token
-
-        if token:
+        if self._auth.linked:  # this triggers keyring access on first call
 
             if not self.client.linked:
                 if self._auth.token_access_type == 'legacy':
-                    self.client.set_token(access_token=token)
+                    self.client.set_token(access_token=self._auth.access_token)
                 else:
-                    self.client.set_token(refresh_token=token)
+                    self.client.set_token(refresh_token=self._auth.refresh_token)
 
             return False
 
