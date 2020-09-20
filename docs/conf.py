5401b03f0e7aa1a1715537e6480bf5a902541b27
Sam Schott <ss2151@cam.ac.uk>
2020-09-20 12:03:12 +0100

    [docs] fade to black

--- docs/conf.py
+++ docs/conf.py
@@ -6,63 +6,65 @@ import os
 import sys
 import time
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../maestral'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../maestral"))
 
 # -- Project information -----------------------------------------------------------------
 
-author = 'Sam Schott'
-version = '1.2.1.dev0'
+author = "Sam Schott"
+version = "1.2.1.dev0"
 release = version
-project = 'Maestral'
-title = 'Maestral API Documentation'
-copyright = '{}, {}'.format(time.localtime().tm_year, author)
+project = "Maestral"
+title = "Maestral API Documentation"
+copyright = "{}, {}".format(time.localtime().tm_year, author)
 
 # -- General configuration ---------------------------------------------------------------
 
 extensions = [
-    'sphinx.ext.napoleon',  # support numpy style docstrings in config module
-    'sphinx.ext.todo',  # support for todo list
-    'sphinx.ext.ifconfig',  # support for if-clauses in docs
-    'sphinx.ext.viewcode',  # link back to github code
-    'autoapi.extension',  # builds API docs from doc strings without importing module
-    'sphinx_click.ext',  # support for click commands
-    'm2r',  # convert markdown to rest
+    "sphinx.ext.napoleon",  # support numpy style docstrings in config module
+    "sphinx.ext.todo",  # support for todo list
+    "sphinx.ext.ifconfig",  # support for if-clauses in docs
+    "sphinx.ext.viewcode",  # link back to github code
+    "autoapi.extension",  # builds API docs from doc strings without importing module
+    "sphinx_click.ext",  # support for click commands
+    "m2r",  # convert markdown to rest
 ]
-source_suffix = ['.rst', '.md']
-master_doc = 'index'
-language = 'en'
+source_suffix = [".rst", ".md"]
+master_doc = "index"
+language = "en"
 # html4_writer = True
 
 # -- Options for HTML output -------------------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
-html_logo = '../maestral/resources/maestral.png'
+html_theme = "sphinx_rtd_theme"
+html_logo = "../maestral/resources/maestral.png"
 html_context = {
-    'css_files': [
-        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-        '_static/custom.css',
+    "css_files": [
+        "https://media.readthedocs.org/css/sphinx_rtd_theme.css",
+        "https://media.readthedocs.org/css/readthedocs-doc-embed.css",
+        "_static/custom.css",
     ],
 }
 
 # -- Options for LaTeX output ------------------------------------------------------------
 
-latex_documents = [(master_doc, 'maestral.tex', title, author, 'manual'), ]
+latex_documents = [
+    (master_doc, "maestral.tex", title, author, "manual"),
+]
 
 # -- Extension configuration -------------------------------------------------------------
 
 # autoapi
-autoapi_type = 'python'
-autoapi_dirs = ['../maestral']
+autoapi_type = "python"
+autoapi_dirs = ["../maestral"]
 autoapi_options = [
-    'members',
-    'inherited-members',
-    'special-members',
-    'show-inheritance',
-    'show-module-summary',
-    'imported-members',
+    "members",
+    "inherited-members",
+    "special-members",
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
 ]
 autoapi_add_toctree_entry = False
 
