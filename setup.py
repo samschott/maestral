48687d8f54218aae2b815a9e08107e2e1041fe11
Sam Schott <ss2151@cam.ac.uk>
2020-10-07 22:37:38 +0100

    bump gui requirement to v1.2.1

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ install_requires = [
 ]
 
 gui_requires = [
-    'maestral_qt>=1.2.0;sys_platform=="linux"',
-    'maestral_cocoa>=1.2.0;sys_platform=="darwin"',
+    'maestral_qt>=1.2.1;sys_platform=="linux"',
+    'maestral_cocoa>=1.2.1;sys_platform=="darwin"',
 ]
 
 syslog_requires = ["systemd-python"]
