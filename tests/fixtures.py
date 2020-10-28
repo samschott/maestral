ceecaf6a339fa2fd4decda20d23bb4b50ec2bfbf
Sam Schott <ss2151@cam.ac.uk>
2020-10-28 12:55:21 +0000

    [tests] move to file-base lock

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -3,13 +3,16 @@
 import os
 import logging
 import time
+from datetime import datetime
+import uuid
 from typing import Optional
 
-from maestral.sync import delete
-from maestral.errors import NotFoundError, FolderConflictError
+from dropbox.files import WriteMode, FileMetadata, FolderMetadata
 from maestral.main import Maestral
+from maestral.errors import NotFoundError, FileConflictError
+from maestral.client import convert_api_errors
 from maestral.utils.housekeeping import remove_configuration
-from maestral.utils.path import generate_cc_name
+from maestral.utils.path import generate_cc_name, delete
 from maestral.utils.appdirs import get_home_dir
 
 
@@ -85,17 +88,27 @@ def cleanup_test_config(m: Maestral, test_folder_dbx: Optional[str] = None) -> N
 class DropboxTestLock:
     """
     A lock on a Dropbox account for running sync tests. The lock will be acquired by
-    create a folder at ``lock_path`` and released by deleting the folder on the remote
+    create a file at ``lock_path`` and released by deleting the file on the remote
     Dropbox. This can be used to synchronise tests running on the same Dropbox account.
+    Lock files older than 1h are considered expired and will be discarded.
 
     :param m: Linked Maestral instance.
     :param lock_path: Path for the lock folder.
+    :param expires_after: The lock will be considered as expired after the given time in
+        seconds since the acquire call. Defaults to 15 min.
     """
 
-    def __init__(self, m: Maestral, lock_path: str = "/test.lock") -> None:
+    def __init__(
+        self,
+        m: Maestral,
+        lock_path: str = "/test.lock",
+        expires_after: float = 15 * 60,
+    ) -> None:
 
         self.m = m
         self.lock_path = lock_path
+        self.expires_after = expires_after
+        self._rev = None
 
     def acquire(self, blocking: bool = True, timeout: float = -1) -> bool:
         """
@@ -105,7 +118,7 @@ class DropboxTestLock:
         does not block. If the lock cannot be acquired, returns False immediately;
         otherwise, sets the lock to locked and returns True.
 
-        :param blocking: Whether to block until the lock can be acquired.s
+        :param blocking: Whether to block until the lock can be acquired.
         :param timeout: Timeout in seconds. If negative, no timeout will be applied.
             If positive, blocking must be set to True.
         :returns: Whether the lock could be acquired (within timeout).
@@ -116,18 +129,53 @@ class DropboxTestLock:
 
         t0 = time.time()
 
+        # we encode the expiry time in the client_modified time stamp
+        expiry_time = datetime.utcfromtimestamp(time.time() + self.expires_after)
+
         while True:
             try:
-                self.m.client.make_dir(self.lock_path)
-            except FolderConflictError:
-                pass
+                with convert_api_errors(dbx_path=self.lock_path):
+                    md = self.m.client.dbx.files_upload(
+                        uuid.uuid4().bytes,
+                        self.lock_path,
+                        mode=WriteMode.add,
+                        client_modified=expiry_time,
+                    )
+                    self._rev = md.rev
+            except FileConflictError:
+                if not self.locked():
+                    continue
             else:
                 return True
 
             if time.time() - t0 > timeout > 0:
                 return False
             else:
-                time.sleep(10)
+                time.sleep(5)
+
+    def locked(self):
+        """
+        Check if locked. Clean up any expired lock files.
+
+        :returns: True if locked, False otherwise.
+        """
+
+        md = self.m.client.get_metadata(self.lock_path)
+
+        if not md:
+            return False
+
+        elif isinstance(md, FileMetadata) and md.client_modified < datetime.utcnow():
+            # lock has expired, remove
+            try:
+                self.m.client.remove(self.lock_path, parent_rev=md.rev)
+            except NotFoundError:
+                # protect against race
+                pass
+
+            return False
+        else:
+            return True
 
     def release(self) -> None:
         """
@@ -136,7 +184,12 @@ class DropboxTestLock:
         :raises: RuntimeError if the lock was not locked.
         """
 
+        if not self._rev:
+            raise RuntimeError("release unlocked lock")
+
         try:
-            self.m.client.remove(self.lock_path)
+            self.m.client.remove(self.lock_path, parent_rev=self._rev)
         except NotFoundError:
             raise RuntimeError("release unlocked lock")
+        else:
+            self._rev = None
