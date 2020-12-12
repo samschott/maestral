1b7ef3178104d720c9dfd0f143928f41d3466052
Sam Schott <ss2151@cam.ac.uk>
2020-12-12 00:17:37 +0000

    [tests] cleaned up offline client tests

--- tests/offline/test_client.py
+++ tests/offline/test_client.py
@@ -7,31 +7,23 @@ from maestral.client import DropboxClient
 from maestral.errors import NotLinkedError
 
 
-def test_client_not_linked():
-    try:
-        client = DropboxClient("test-config")
-
-        assert not client.linked
-
-        with pytest.raises(NotLinkedError):
-            client.get_account_info()
-    finally:
-        remove_configuration("test-config")
+@pytest.fixture
+def client():
+    yield DropboxClient("test-config")
+    remove_configuration("test-config")
 
 
-def test_auth_url():
-    try:
-        client = DropboxClient("test-config")
-        url = client.get_auth_url()
-        assert url.startswith("https://www.dropbox.com/oauth2/authorize?")
-    finally:
-        remove_configuration("test-config")
+def test_client_not_linked(client):
+    assert not client.linked
+
+    with pytest.raises(NotLinkedError):
+        client.get_account_info()
 
 
-def test_auth_error():
-    try:
-        client = DropboxClient("test-config")
-        assert client.link("invalid-token") == 1
+def test_auth_url(client):
+    url = client.get_auth_url()
+    assert url.startswith("https://www.dropbox.com/oauth2/authorize?")
 
-    finally:
-        remove_configuration("test-config")
+
+def test_auth_error(client):
+    assert client.link("invalid-token") == 1
