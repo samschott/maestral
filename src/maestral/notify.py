90cade064c4be38a8a04b75273202a8ee7f30f35
Sam Schott <ss2151@cam.ac.uk>
2021-01-20 11:58:20 +0000

    [notify] move generic code to separate package

--- src/maestral/notify.py
+++ src/maestral/notify.py
@@ -6,14 +6,14 @@ the platform.
 
 # system imports
 import time
-import platform
-from threading import Lock
 from typing import Optional, Dict, Callable
 
+# external imports
+from desktop_notifier import DesktopNotifier, NotificationLevel
+
 # local imports
-from ..config import MaestralConfig
-from ..constants import APP_NAME, APP_ICON_PATH
-from .base import DesktopNotifierBase, NotificationLevel, Notification
+from .config import MaestralConfig
+from .constants import APP_NAME, APP_ICON_PATH
 
 
 __all__ = [
@@ -23,69 +23,8 @@ __all__ = [
     "FILECHANGE",
     "level_name_to_number",
     "level_number_to_name",
-    "Notification",
-    "NotificationLevel",
-    "DesktopNotifier",
     "MaestralDesktopNotifier",
 ]
-
-
-class DesktopNotifier:
-    """Cross-platform desktop notification emitter
-
-    Uses different backends depending on the platform version and available services.
-    The Dbus backend requires a running asyncio loop. The Cocoa implementations will
-    dispatch notifications without an event loop but require a running CFRunLoop *in the
-    main thread* to react to user interactions with the notification. Packages such as
-    :mod:`rubicon.objc` can be used to integrate asyncio with a CFRunLoop.
-
-    :param app_name: Name of app which sends notifications.
-    """
-
-    _impl: DesktopNotifierBase
-
-    def __init__(self, app_name: str = "Python") -> None:
-        self._lock = Lock()
-
-        if platform.system() == "Darwin":
-            from .macos import Impl
-        elif platform.system() == "Linux":
-            from .linux import Impl  # type: ignore
-        else:
-            Impl = DesktopNotifierBase  # type: ignore
-
-        self._impl = Impl(app_name)
-
-    def send(
-        self,
-        title: str,
-        message: str,
-        urgency: NotificationLevel = NotificationLevel.Normal,
-        icon: Optional[str] = None,
-        action: Optional[Callable] = None,
-        buttons: Optional[Dict[str, Callable]] = None,
-    ) -> None:
-        """
-        Sends a desktop notification. Some arguments may be ignored, depending on the
-        backend.
-
-        :param title: Notification title.
-        :param message: Notification message.
-        :param urgency: Notification level: low, normal or critical. This is ignored by
-            some implementations.
-        :param icon: Path to an icon to use for the notification, typically the app
-            icon. This is ignored by some implementations, e.g., on macOS where the icon
-            of the app bundle is always used.
-        :param action: Handler to call when the notification is clicked. This is ignored
-            by some implementations.
-        :param buttons: A dictionary with button names and callbacks to show in the
-            notification. This is ignored by some implementations.
-        """
-        notification = Notification(title, message, urgency, icon, action, buttons)
-
-        with self._lock:
-            self._impl.send(notification)
-
 
 _desktop_notifier = DesktopNotifier(APP_NAME)
 
