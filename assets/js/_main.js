6f93297f861140399ed97c327429baf0fa341723
Sam Schott <ss2151@cam.ac.uk>
2021-02-25 20:10:45 +0000

    remove vendored sass and js where not needed

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -13,9 +13,6 @@ $(document).ready(function() {
     $btn.toggleClass("close");
   });
 
-  // FitVids init
-  $("#main").fitVids();
-
   // Sticky sidebar
   var stickySideBar = function() {
     var show =
@@ -62,85 +59,6 @@ $(document).ready(function() {
     setTimeout(function() {
       $(".search-content input").focus();
     }, 400);
-  });
-
-  // Smooth scrolling
-
-  header = document.querySelector(".masthead")
-  headerStyle = getComputedStyle(header)
-
-  // accomodate sticky header
-  if ( headerStyle.position === "sticky" ) {
-    scrollOffset = header.offsetHeight
-  } else {
-    scrollOffset = 0
-  }
-
-  var scroll = new SmoothScroll('a[href*="#"]', {
-    offset: scrollOffset,
-    speed: 400,
-    speedAsDuration: true,
-    durationMax: 500
-  });
-
-  // Gumshoe scroll spy init
-  if($("nav.toc").length > 0) {
-    var spy = new Gumshoe("nav.toc a", {
-      // Active classes
-      navClass: "active", // applied to the nav list item
-      contentClass: "active", // applied to the content
-
-      // Nested navigation
-      nested: false, // if true, add classes to parents of active link
-      nestedClass: "active", // applied to the parent items
-
-      // Offset & reflow
-      offset: 20, // how far from the top of the page to activate a content area
-      reflow: true, // if true, listen for reflows
-
-      // Event support
-      events: true // if true, emit custom events
-    });
-  }
-
-  // add lightbox class to all image links
-  $(
-    "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']"
-  ).addClass("image-popup");
-
-  // Magnific-Popup options
-  $(".image-popup").magnificPopup({
-    // disableOn: function() {
-    //   if( $(window).width() < 500 ) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
-    type: "image",
-    tLoading: "Loading image #%curr%...",
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
-    },
-    removalDelay: 500, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: "mfp-zoom-in",
-    callbacks: {
-      beforeOpen: function() {
-        // just a hack that adds mfp-anim class to markup
-        this.st.image.markup = this.st.image.markup.replace(
-          "mfp-figure",
-          "mfp-figure mfp-with-anim"
-        );
-      }
-    },
-    closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
   // Add anchors for headings
