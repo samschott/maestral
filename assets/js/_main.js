fa480b869e57181ab399e351eb4953da2702c1cd
Sam Schott <ss2151@cam.ac.uk>
2021-02-28 02:00:33 +0000

    don't use javascript for burger nav

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,15 +4,6 @@
 
 onready = function() {
 
-  // Toggle navigation when burger is clicked
-  var btn = document.getElementById("gn-toggle");
-  var hlinks = document.getElementById("gn-hidden-link");
-
-  btn.onclick = function() {
-    hlinks.classList.toggle("collapsed");
-    btn.classList.toggle("close");
-  };
-
   // Search
 
   var initial_content = document.getElementsByClassName("initial-content").item(0);
@@ -48,14 +39,14 @@ onready = function() {
   // Add anchors for all headings with id
 
   var page__content = document.getElementsByClassName("page__content").item(0);
-  var headings = page__content.querySelectorAll('h1, h2, h3, h4, h5, h6');
+  var headings = page__content.querySelectorAll("h1, h2, h3, h4, h5, h6");
   headings.forEach($heading => {
-    var id = $heading.getAttribute('id');
+    var id = $heading.getAttribute("id");
     if (id) {
       var anchor = document.createElement("a");
-      anchor.className = 'header-link';
-      anchor.href = '#' + id;
-      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>';
+      anchor.className = "header-link";
+      anchor.href = "#" + id;
+      anchor.innerHTML = "<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>";
       anchor.title = "Permalink";
       $heading.append(anchor);
     }
