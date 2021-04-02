4f0f6874472b50bb7c0dd5d2339a2bf5cd6361a3
Sam Schott <ss2151@cam.ac.uk>
2021-04-02 19:05:28 +0100

    [config] bumped to 16.0.0

--- src/maestral/config/main.py
+++ src/maestral/config/main.py
@@ -77,7 +77,7 @@ DEFAULTS_STATE: DefaultsType = {
 #    or if you want to *rename* options, then you need to do a MAJOR update in
 #    version, e.g. from 3.0.0 to 4.0.0
 # 3. You don't need to touch this value if you're just adding a new option
-CONF_VERSION = "15.0.0"
+CONF_VERSION = "16.0.0"
 
 
 # =============================================================================
