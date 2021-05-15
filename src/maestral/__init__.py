9b9d7546136aea00c5f78c4a1b799540dbebb92f
Sam Schott <ss2151@cam.ac.uk>
2021-05-15 13:32:45 +0100

    drop www subdomain from url

--- src/maestral/__init__.py
+++ src/maestral/__init__.py
@@ -4,7 +4,7 @@ import warnings
 
 __version__ = "1.4.4.dev1"
 __author__ = "Sam Schott"
-__url__ = "https://www.maestral.app"
+__url__ = "https://maestral.app"
 
 
 # suppress Python 3.9 warning from rubicon-objc
