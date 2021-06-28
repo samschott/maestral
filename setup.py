3d867ed461a81bcb72783693ab310e3c7fa6d2fb
Sam Schott <ss2151@cam.ac.uk>
2021-06-28 23:51:25 +0100

    bump gui dependency

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ install_requires = [
 ]
 
 gui_requires = [
-    "maestral-qt>=1.4.4;sys_platform=='linux'",
-    "maestral-cocoa>=1.4.4;sys_platform=='darwin'",
+    "maestral-qt>=1.4.5;sys_platform=='linux'",
+    "maestral-cocoa>=1.4.5;sys_platform=='darwin'",
 ]
 
 syslog_requires = ["systemd-python"]
