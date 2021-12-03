103fa989b040e503d8759b46aa9e9167d87497a2
Sam Schott <ss2151@cam.ac.uk>
2021-12-03 19:56:01 +0100

    remove click shell completion patch

--- src/maestral/__init__.py
+++ src/maestral/__init__.py
@@ -2,8 +2,6 @@
 
 import warnings
 
-from click import shell_completion  # type: ignore
-
 
 __version__ = "1.5.3.dev0"
 __author__ = "Sam Schott"
@@ -12,15 +10,3 @@ __url__ = "https://maestral.app"
 
 # suppress Python 3.9 warning from rubicon-objc
 warnings.filterwarnings("ignore", module="rubicon", category=UserWarning)
-
-
-# patch click shell completion argument detection
-# see https://github.com/pallets/click/issues/1929
-
-
-def _start_of_option(value: str) -> bool:
-    """Check if the value looks like the start of an option."""
-    return value[0] == "-" if value else False
-
-
-shell_completion._start_of_option = _start_of_option
