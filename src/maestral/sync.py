34a4c8f6ea0d86be3988f301fcd0b440b1e7a9e8
Sam Schott <ss2151@cam.ac.uk>
2021-12-15 22:35:54 +0100

    [sync] show file also when clicking the notification itself
    see #547

--- src/maestral/sync.py
+++ src/maestral/sync.py
@@ -3097,6 +3097,9 @@ class SyncEngine:
             else:
                 file_name = f"{n_changed} items"
 
+            def callback():
+                pass
+
             buttons = {}
 
         if change_type == ChangeType.Removed.value:
@@ -3112,7 +3115,9 @@ class SyncEngine:
         else:
             msg = f"{file_name} {change_type}"
 
-        self.desktop_notifier.notify("Items synced", msg, actions=buttons)
+        self.desktop_notifier.notify(
+            "Items synced", msg, actions=buttons, on_click=callback
+        )
 
     def _check_download_conflict(self, event: SyncEvent) -> Conflict:
         """
