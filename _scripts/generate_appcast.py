6a41745b9d6f4e701fbc85a3de1491f5fed528cc
samschott <sam.schott@outlook.com>
2022-06-07 00:51:48 +0200

    appcast script generates nicer formatting

--- _scripts/generate_appcast.py
+++ _scripts/generate_appcast.py
@@ -5,9 +5,10 @@ A script that generates an appcast from GitHub releases.
 
 import os
 import shlex
-import shutil
 import plistlib
+import tempfile
 import subprocess
+import textwrap
 
 import requests
 
@@ -34,58 +35,97 @@ DOWNLOAD_FOLDER_ABSOLUTE = os.path.join(os.getcwd(), DOWNLOAD_FOLDER)
 
 SPARKLE_PRIVATE_KEY = os.environ.get("SPARKLE_PRIVATE_KEY")
 
+APPCAST_XML = """\
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"  xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <channel>
+    <title>{app_name} Update Feed</title>
+    <link>{appcast_url}</link>
+    <description>Stable releases of {app_name}</description>
+    <language>en</language>
+{items_joined}
+  </channel>
+</rss>"""
+
+APPCAST_XML_PRE = """\
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"  xmlns:dc="http://purl.org/dc/elements/1.1/">
+  <channel>
+    <title>{app_name} Update Feed for Prereleases</title>
+    <link>{appcast_url}</link>
+    <description>Prereleases of {app_name}</description>
+    <language>en</language>
+{items_joined}
+  </channel>
+</rss>"""
+
+APPCAST_ITEM_XML = """\
+<item>
+  <title>{title}</title>
+  <pubDate>{publising_date}</pubDate>
+  <sparkle:version>{build_number}</sparkle:version>
+  <sparkle:shortVersionString>{short_version_str}</sparkle:shortVersionString>
+  <sparkle:minimumSystemVersion>{min_system}</sparkle:minimumSystemVersion>
+  <description><![CDATA[
+{release_notes}
+  ]]>
+  </description>
+  <enclosure
+    url="{download_link}"
+    {signature_and_length}
+    type="application/octet-stream"/>
+</item>"""
+
 
 def generate():
-    try:
+    resp = requests.get(GITHUB_RELEASES_API)
+    releases = resp.json()
 
-        resp = requests.get(GITHUB_RELEASES_API)
-        releases = resp.json()
+    appcast_items = []
+    appcast_pre_items = []
 
-        appcast_items = []
-        appcast_pre_items = []
+    # Iterate over the last 5 releases and collect data for the appcast.
+    # TODO: Add only new releases to app-cast.
 
-        # Iterate over the last 5 releases and collect data for the appcast.
-        # TODO: Add only new releases to app-cast.
+    for r in releases[:5]:
 
-        for r in releases[:5]:
+        # Get short version.
+        release_name = r["name"]
 
-            # Get short version
-            release_name = r["name"]
+        print(f"Processing release {release_name}...")
 
-            print(f"Processing release {release_name}...")
+        # Get release notes.
+        release_notes = r["body"]  # This is markdown
 
-            # Get release notes
-            release_notes = r["body"]  # This is markdown
+        # Write release notes to file.
+        os.makedirs(DOWNLOAD_FOLDER_ABSOLUTE, exist_ok=True)
 
-            # Write release notes to file.
-            os.makedirs(DOWNLOAD_FOLDER_ABSOLUTE, exist_ok=True)
+        with open(f"{DOWNLOAD_FOLDER}/release_notes.md", "w") as f:
+            f.write(release_notes)
 
-            with open(f"{DOWNLOAD_FOLDER}/release_notes.md", "w") as f:
-                f.write(release_notes)
+        # Convert to HTML.
+        release_notes = subprocess.check_output(
+            f"cat {DOWNLOAD_FOLDER}/release_notes.md | pandoc -f markdown -t html -c '' -H {RELEASE_NOTES_CSS} -s --metadata title='{release_name}'",
+            stderr=subprocess.STDOUT,
+            shell=True,
+        ).decode()
 
-            # Convert to HTML
-            release_notes = subprocess.check_output(
-                f"cat {DOWNLOAD_FOLDER}/release_notes.md | pandoc -f markdown -t html -c '' -H {RELEASE_NOTES_CSS} -s --metadata title='{release_name}'",
-                stderr=subprocess.STDOUT,
-                shell=True,
-            ).decode()
+        # Get title.
+        title = f"{release_name} available"
 
-            # Get title
-            title = f"{release_name} available"
+        # Get publishing date.
+        publising_date = r["published_at"]
 
-            # Get publishing date
-            publising_date = r["published_at"]
+        # Get isPrerelease.
+        is_prerelease = r["prerelease"]
 
-            # Get isPrerelease
-            is_prerelease = r["prerelease"]
+        # Get download link.
+        download_link = r["assets"][0]["browser_download_url"]
 
-            # Get download link
-            download_link = r["assets"][0]["browser_download_url"]
-
+        with tempfile.TemporaryDirectory() as tmpdir:
             # Download dmg
-            os.makedirs(DOWNLOAD_FOLDER_ABSOLUTE, exist_ok=True)
             download_name = download_link.rsplit("/", 1)[-1]
-            download_path = f"{DOWNLOAD_FOLDER}/{download_name}"
+            download_path = f"{tmpdir}/{download_name}"
 
             res = requests.get(download_link, allow_redirects=True)
 
@@ -106,7 +146,7 @@ def generate():
             min_system = info["LSMinimumSystemVersion"]
 
             if "SUPublicEDKey" not in info:
-                # skip all pre-sparkle app bundles
+                print(f"Skipping {release_name}, no SUPublicEDKey in Info.plist")
                 continue
 
             # Get edSignature
@@ -115,77 +155,50 @@ def generate():
             if SPARKLE_PRIVATE_KEY:
                 cmd += f" -s {SPARKLE_PRIVATE_KEY}"
 
-            signature_and_length = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT).decode()
+            signature_and_length = subprocess.check_output(
+                cmd, shell=True, stderr=subprocess.STDOUT
+            ).decode()
             signature_and_length = signature_and_length.strip("\n")
 
-            # Assemble collected data into appcast.xml-ready item-string
-            item_string = f"""\
-    <item>
-        <title>{title}</title>
-        <pubDate>{publising_date}</pubDate>
-        <sparkle:version>{build_number}</sparkle:version>
-        <sparkle:shortVersionString>{short_version_str}</sparkle:shortVersionString>
-        <sparkle:minimumSystemVersion>{min_system}</sparkle:minimumSystemVersion>
-        <description><![CDATA[
-            {release_notes}
-        ]]>
-        </description>
-        <enclosure
-            url="{download_link}"
-            {signature_and_length}
-            type="application/octet-stream"
-        />
-    </item>"""
+        # Assemble collected data into appcast.xml-ready item-string.
+        item_string = APPCAST_ITEM_XML.format(
+            title=title,
+            publising_date=publising_date,
+            build_number=build_number,
+            short_version_str=short_version_str,
+            min_system=min_system,
+            release_notes=textwrap.indent(release_notes, 4 * " "),
+            download_link=download_link,
+            signature_and_length=signature_and_length,
+        )
 
-            # Append item_string to arrays
-            if not is_prerelease:
-                appcast_items.append(item_string)
+        # Append item_string to arrays.
+        appcast_pre_items.append(item_string)
 
-            appcast_pre_items.append(item_string)
+        if not is_prerelease:
+            appcast_items.append(item_string)
 
-        # Clean up downloaded files
-        shutil.rmtree(DOWNLOAD_FOLDER, ignore_errors=True)
+    # Assemble final appcast xml.
+    items_joined = "\n".join(appcast_items)
+    pre_items_joined = "\n".join(appcast_pre_items)
 
-        # Assemble item strings into final appcast strings
+    appcast_content_string = APPCAST_XML.format(
+        app_name=APP_NAME,
+        appcast_url=APPCAST_URL,
+        items_joined=textwrap.indent(items_joined, prefix=4 * " "),
+    )
+    appcast_pre_content_string = APPCAST_XML_PRE.format(
+        app_name=APP_NAME,
+        appcast_url=APPCAST_PRE_URL,
+        items_joined=textwrap.indent(pre_items_joined, prefix=4 * " "),
+    )
 
-        items_joined = "\n".join(appcast_items)
-        pre_items_joined = "\n".join(appcast_pre_items)
+    # Write to file.
+    with open(APPCAST_FILENAME, "w") as f:
+        f.write(appcast_content_string)
 
-        appcast_content_string = f"""\
-<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"  xmlns:dc="http://purl.org/dc/elements/1.1/">
-  <channel>
-    <title>{APP_NAME} Update Feed</title>
-    <link>{APPCAST_URL}</link>
-    <description>Stable releases of {APP_NAME}</description>
-    <language>en</language>
-    {items_joined}
-  </channel>
-</rss>"""
-
-        appcast_pre_content_string = f"""\
-<?xml version="1.0" encoding="utf-8"?>
-<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle"  xmlns:dc="http://purl.org/dc/elements/1.1/">
-  <channel>
-    <title>{APP_NAME} Update Feed for Prereleases</title>
-    <link>{APPCAST_PRE_URL}</link>
-    <description>Prereleases of {APP_NAME}</description>
-    <language>en</language>
-    {pre_items_joined}
-  </channel>
-</rss>"""
-
-        # Write to file
-
-        with open(APPCAST_FILENAME, "w") as f:
-            f.write(appcast_content_string)
-
-        with open(APPCAST_PRE_FILENAME, "w") as f:
-            f.write(appcast_pre_content_string)
-
-    finally:
-        # Clean up download folder
-        shutil.rmtree(DOWNLOAD_FOLDER, ignore_errors=True)
+    with open(APPCAST_PRE_FILENAME, "w") as f:
+        f.write(appcast_pre_content_string)
 
 
 if __name__ == "__main__":
