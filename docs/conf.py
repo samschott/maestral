7b4530d49354e4eac60b4f7f67a0a11f684a145c
Sam Schott <sam.schott@outlook.com>
2023-05-12 22:25:07 +0200

    Bump version: 1.7.2 → 1.7.3.dev0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 # -- Project information ---------------------------------------------------------------
 
 author = "Sam Schott"
-version = "1.7.2"
+version = "1.7.3.dev0"
 release = version
 project = "Maestral"
 title = "Maestral API Documentation"
